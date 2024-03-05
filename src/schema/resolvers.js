fc350f3dfccd803051cd2dccbc316a0aec6c00d7
David Lafontant <lafontant.david@gmail.com>
2024-03-05 07:30:09 -0500

    Replace jsonData by usersData

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,22 +1,25 @@
 const fs = require('fs');
 const _ = require('lodash');
 
-let jsonData = null;
+let usersData = null;
 fs.readFile('src/data/users.json', 'utf-8', (err, data) => {
   if (err) { throw new Error(err.message); }
-  jsonData = JSON.parse(data);
+  usersData = JSON.parse(data);
 });
 
 const resolvers = {
   Query: {
     users() {
-      return jsonData;
+      return usersData;
     },
     user(parent, args) {
       const { id } = args;
-      const user = _.find(jsonData, { id: Number(id) });
+      const user = _.find(usersData, { id: Number(id) });
       return user;
     },
+    movies() {
+      reti
+    }
   },
 };
 
