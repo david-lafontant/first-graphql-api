f9e808efd4f15be2dfb3ca0ae10a0863840aca27
David Lafontant <lafontant.david@gmail.com>
2024-03-05 07:25:50 -0500

    Move files in src folder

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,9 +1,8 @@
-// const data = require('../data.json');
 const fs = require('fs');
 const _ = require('lodash');
 
 let jsonData = null;
-fs.readFile('data.json', 'utf-8', (err, data) => {
+fs.readFile('src/data/users.json', 'utf-8', (err, data) => {
   if (err) { throw new Error(err.message); }
   jsonData = JSON.parse(data);
 });
